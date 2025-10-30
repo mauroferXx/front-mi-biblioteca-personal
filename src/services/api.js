8f6402b54bfd49e6e612421a51224b40d821828e
mauroferXx <101601825+mauroferXx@users.noreply.github.com>
2025-10-30 09:26:00 -0300

    api connect

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://trivia-prod.eba-8n5wucxt.us-east-2.elasticbeanstalk.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
